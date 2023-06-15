da610b182f8da516ea21359a04c196912d3c921f
ekanur <eka.html@gmail.com>
2023-06-16 01:06:20 +0700

    front

---
+++ resources/views/front/layout/layout.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/pengunjung.css" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+
+    <title>E-DATA | Pengunjung</title>
+  </head>
+  <body style="background-color: #f2f2f8">
+    <!-- Logo & banner -->
+    <section class="header align-items-center d-flex justify-content-center">
+      <img src="images/logo.png" height="50%" />
+    </section>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-white p-0">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item me-2">
+              <a class="nav-link active text-white bg-primary" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item me-2">
+              <a class="nav-link text-dark ps-3 pe-3" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2 form-control-sm custom-font" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-primary btn-sm custom-font" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <!-- Main  -->
+    <section>
+      <div class="card mt-3 mb-3 custom-margin border-0" style="box-shadow: 0 2px 6px rgba(0, 0, 0, 0.16)">
+        <div class="card-body pe-5 ps-5 pt-3 pb-2">
+          <!-- t&c title -->
+          <h3 class="card-title text-center mt-1 mb-2">Term & condition</h3>
+          <!-- t&c  -->
+          <div class="overflow-auto tc-content">
+            <h5 class="pb-2 m-0 fw-normal">First heading</h5>
+            <p class="pe-3 custom-font text-secondary">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil placeat blanditiis hic facere maiores, minima saepe ratione autem explicabo nesciunt ea eos sed velit ex odit voluptas
+              consequuntur reiciendis aspernatur quod modi illum illo culpa totam doloremque. Nesciunt facilis aliquam rem corrupti sapiente eveniet? Dignissimos nobis possimus, laudantium aliquid vel
+              animi nam id maxime omnis tempora distinctio odio provident consequatur dicta? Qui, illo repellendus? Maxime quasi sed recusandae tempora eligendi esse! Quae explicabo fugiat,
+              dignissimos illo esse quas quisquam quos consequuntur debitis quis cupiditate laudantium non nostrum sed ducimus praesentium earum veritatis maiores nobis velit! Eos deleniti qui libero
+              ipsa sapiente laudantium, sequi numquam, molestiae aperiam iusto impedit praesentium vel! Perspiciatis dignissimos ipsum quas adipisci culpa optio eveniet, laboriosam corporis, quia,
+              asperiores exercitationem molestias nesciunt tempora assumenda eum dolore mollitia rem? Ipsa, non. Amet aliquid magni aperiam vitae, ullam necessitatibus temporibus quia cumque! Quia nam
+              ipsa temporibus! Saepe culpa nisi veniam, eum fuga possimus corrupti sunt eius ducimus ratione ullam! Vel expedita fugiat omnis esse consequuntur animi fuga quia deleniti nulla placeat?
+              Vero, laudantium. Officiis doloribus, odio facere obcaecati rem nam dignissimos quam adipisci quo quibusdam ipsa, sapiente illo ad. Esse animi vitae repudiandae culpa nobis, corporis,
+              rerum tenetur id odio vel fuga omnis eos reprehenderit tempore quam nam ipsam voluptatem doloribus ipsum sunt eaque officia commodi? Labore aliquam provident blanditiis vitae inventore
+              unde incidunt dolores eum placeat autem eligendi, amet nesciunt odit sit adipisci porro, ipsam optio tempora ipsa rerum, quia ut laudantium sed esse! Incidunt cumque nulla debitis in
+              minus dolorem, sint autem quia accusamus ad asperiores quos dignissimos maxime porro, quam iste! Architecto similique in facilis magni vel quasi temporibus. Nesciunt, temporibus
+              laboriosam. Sapiente ipsum error, mollitia repellendus sit, officia veritatis aliquid quo, ea alias fugit ducimus repudiandae assumenda? Quaerat vitae, perspiciatis tempore repellat
+              aliquid voluptate ea tempora quibusdam reprehenderit. Alias blanditiis quidem, laborum facilis eius inventore enim illo, cupiditate velit voluptas qui laboriosam quibusdam accusantium
+              perferendis. Autem vero magni quae possimus? Unde ipsum placeat facilis quam. Officiis, ex nihil dolorum enim possimus odio quos nostrum in, modi soluta quam natus iste sunt est, vitae
+              deleniti delectus at earum quas sequi pariatur. Aliquam quos enim, rem voluptatibus at earum harum quod delectus culpa natus, magni repellat tempora sequi cumque quo animi possimus nulla
+              itaque! Facere qui officiis accusamus aspernatur aut nihil harum deleniti rem quos dignissimos molestiae, labore, provident totam error nisi nostrum. Quia autem sunt porro excepturi
+              similique tempora atque inventore error quo, harum quas fugit velit! Omnis suscipit labore eos dolorum quo sint adipisci laudantium assumenda quisquam eum ad veritatis ab, nemo alias
+              velit eius ullam sit ipsa soluta odit expedita facilis sapiente, tempora doloribus? Excepturi maiores quidem perferendis dolor iure quae maxime earum! Velit est perspiciatis repudiandae
+              amet aperiam, veniam qui sint! Nam qui nesciunt repellat error voluptate id repellendus esse maxime expedita? Placeat, voluptas. Sint voluptatibus excepturi cumque architecto sit in
+              voluptatum earum molestiae repellat mollitia laudantium similique minus libero incidunt fuga, ex eveniet illum nulla harum. Similique laudantium ab autem explicabo rerum eum, sed illo
+              eaque, ratione rem ex quasi officia neque aliquid error! Minus illo animi hic voluptatibus repellendus culpa dolor sit laborum fugit labore minima, magni adipisci quod, aperiam, corrupti
+              exercitationem? Atque tenetur nesciunt consequuntur aliquam unde accusamus totam obcaecati, nulla dolores, nobis blanditiis dignissimos molestiae hic dolore quos. Quo iste molestiae iure
+              earum officia. Rem iste, provident ipsa iure consectetur blanditiis quasi inventore? Iure amet ab sint nihil voluptates consectetur distinctio delectus aliquid eos, fugiat ex a excepturi
+              aliquam facere consequuntur repellendus temporibus veniam dolores eveniet laudantium quisquam assumenda modi non suscipit. Sunt, porro pariatur dolorum suscipit quis obcaecati cum qui
+              impedit accusamus labore praesentium? Vero, illum! Dignissimos mollitia, illo alias enim error esse ab similique suscipit repudiandae non obcaecati magni aspernatur neque blanditiis! A
+              quibusdam aperiam saepe dolorem, consequatur velit accusantium iure praesentium labore porro, maxime esse? Repellendus odit non sunt quae, reiciendis laboriosam? Culpa sunt maxime
+              quibusdam rerum modi ducimus ut? Dignissimos molestias commodi provident in quisquam placeat saepe, deserunt magni magnam amet, ad, assumenda mollitia quod ducimus quis ea alias id
+              dolore praesentium expedita optio dolorum facilis. Dolor reiciendis veniam dolore repellendus, aperiam quia quo incidunt provident laboriosam molestias voluptatum itaque. Quidem debitis
+              molestiae ipsum. Voluptas quos totam alias qui, est maiores non, nesciunt, facere rerum aperiam quia similique repudiandae deleniti quas quisquam voluptates assumenda eligendi delectus
+              asperiores! Maiores dolorum enim sequi accusamus, reiciendis commodi hic nisi similique? Ea mollitia molestias, esse, earum consequatur deserunt ad, nihil consequuntur natus modi maxime
+              et? Et tempore fugit, expedita molestiae iusto impedit quam repellendus! Pariatur reiciendis consequatur nesciunt? Neque rem, aliquam voluptates sapiente, necessitatibus explicabo aut
+              ducimus repudiandae minima earum architecto consequatur debitis fugiat! Voluptatibus minus veniam commodi quas exercitationem perferendis impedit provident est tenetur deleniti minima
+              quaerat incidunt, quidem ut delectus illo consequuntur recusandae facere at odit accusamus aspernatur. Itaque, reprehenderit corrupti libero, ea molestiae atque quidem suscipit aut quos
+              fuga ut enim ipsam architecto aliquid, perspiciatis saepe tempore sit voluptatum aspernatur? Amet sed sequi obcaecati et numquam quae ad. Laudantium, ullam nostrum! Perspiciatis delectus
+              minus deserunt. Nisi incidunt et ipsum, accusamus rerum deserunt deleniti dignissimos nam praesentium odit molestiae minima nemo distinctio vero, magnam eligendi illo placeat voluptatem.
+              Consequatur consectetur voluptates ut et quia atque explicabo repellendus quos fugit! Eaque libero perspiciatis amet ex facilis numquam asperiores excepturi architecto corporis! A,
+              expedita. Officia quas iure ea aspernatur laboriosam. Quisquam consequuntur cumque neque sapiente ipsa magni at ducimus, aliquam ab beatae repudiandae dolores architecto eius explicabo
+              eaque quam unde molestias numquam reprehenderit quaerat? Officia voluptas doloremque magni optio excepturi nesciunt fugiat possimus quo? Voluptate cumque nulla, consequatur aliquam
+              voluptatum quia nisi quaerat mollitia accusantium hic illum repellendus molestias deserunt veniam voluptatibus inventore recusandae minima ratione, saepe temporibus. Ipsam illum unde
+              voluptates, itaque exercitationem molestiae modi quod rem vel qui nulla eius. Nesciunt, ullam dolores est ad voluptatem enim cum, repudiandae, eius porro quod quis quam numquam placeat
+              sapiente alias sunt quo! Consequatur nobis eius alias eum dolores voluptatem tenetur temporibus laboriosam a commodi repudiandae, accusamus ut sequi consequuntur.
+            </p>
+          </div>
+          <!-- checbox & button  -->
+          <div class="form-check text-center d-flex align-items-center mt-2">
+            <input class="form-check-input" type="checkbox" value="" id="Checkbox" />
+            <label class="form-check-label ms-2 me-2" for="Checkbox"> I Agree</label>
+            <div class="flex-grow-1"></div>
+            <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#formModal">UPLOAD</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- modal coba -->
+    <div class="modal fade" id="formModal" role="dialog" aria-labelledby="modalLable" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header"></div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer d-flex justify-content-center align-content-center">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CLOSE</button>
+            <button type="button" class="btn btn-primary">NEXT</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer  -->
+    <footer class="footer w-100 shadow-sm custom-font bg-white d-flex justify-content-center align-items-center">
+      <span class="fs-8 fw-lighter">Copyright © 2023 - Developed with Laravel</span>
+    </footer>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>

15aa6df5eb944e36d3de10f889cfdcb4afd22e32
ekanur <eka.html@gmail.com>
2023-06-27 13:20:02 +0700

    dashboad admin statis

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,7 +16,7 @@
 
 @section('content')
 <div class="row">
-  <div class="col-lg-8 mb-4 order-0">
+  <div class="col-md-8 col-lg-8 mb-4 order-0">
     <div class="card">
       <div class="d-flex align-items-end row">
         <div class="col-sm-7">
@@ -37,53 +37,37 @@
   </div>
   <div class="col-lg-4 col-md-4 order-1">
     <div class="row">
-      <div class="col-lg-6 col-md-12 col-6 mb-4">
+      <div class="col-12 mb-4">
         <div class="card">
           <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-              
-                <span class="fw-semibold d-block mb-1">Dokumen</span>
-                {{-- <img src="{{asset('assets/img/icons/unicons/chart-success.png')}}" alt="chart success" class="rounded"> --}}
-              
-              {{-- <div class="dropdown">
-                <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="bx bx-dots-vertical-rounded"></i>
-                </button>
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                  <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                  <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+            <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
+              <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
+                <div class="card-title">
+                  <h5 class="text-nowrap mb-2">Dokumen Upload</h5>
+                  <span class="badge bg-label-warning rounded-pill">Hari ini</span>
                 </div>
-              </div> --}}
+                <div class="mt-sm-auto">
+                  {{-- <small class="text-success text-nowrap fw-semibold"><i class='bx bx-chevron-up'></i> 10%</small> --}}
+                  <h3 class="mb-0">15</h3>
+                </div>
+              </div>
+              <div id="profileReportChart"></div>
             </div>
-            
-            <h3 class="card-title mb-2">342</h3>
-            <small class="text-success fw-semibold"> telah diupload</small>
           </div>
         </div>
       </div>
-      <div class="col-lg-6 col-md-12 col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="fw-semibold d-block mb-1">Dokumen</span>
-            </div>
-            
-            <h3 class="card-title text-nowrap mb-1">13</h3>
-            <small class="text-success fw-semibold">Upload hari ini</small>
-          </div>
-        </div>
-      </div>
+      
     </div>
   </div>
   <!-- Total Revenue -->
   <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
     <div class="card">
       <div class="row row-bordered g-0">
-        <div class="col-md-8">
-          <h5 class="card-header m-0 me-2 pb-3">Total Revenue</h5>
+        <div class="col-md-12">
+          <h5 class="card-header m-0 me-2 pb-3">Dokumen Berdasarkan Program Studi</h5>
           <div id="totalRevenueChart" class="px-2"></div>
         </div>
-        <div class="col-md-4">
+        {{-- <div class="col-md-4">
           <div class="card-body">
             <div class="text-center">
               <div class="dropdown">
@@ -121,190 +105,127 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> --}}
       </div>
     </div>
   </div>
   <!--/ Total Revenue -->
-  <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+  <div class="col-12 col-8 col-lg-4 order-3 order-md-2">
     <div class="row">
-      <div class="col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="d-block mb-1">Tugas Akhir</span>
+      <div class="col-md-12 col-lg-12 col-xl-12 order-0 mb-4">
+        <div class="card h-100">
+          <div class="card-header d-flex align-items-center justify-content-between pb-0">
+            <div class="card-title mb-0">
+              <h5 class="m-0 me-2">Statistik Dokumen</h5>
+              {{-- <small class="text-muted">357 Total Dokumen</small> --}}
             </div>
-            
-            <h3 class="card-title text-nowrap mb-2">154</h3>
-            <small class="text-success fw-semibold">Dokumen</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="fw-semibold d-block mb-1">Skripsi</span>
-            </div>
-            
-            <h3 class="card-title mb-2">98</h3>
-            <small class="text-success fw-semibold">Dokumen</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="fw-semibold d-block mb-1">Artikel</span>
-            </div>
-            
-            <h3 class="card-title mb-2">77</h3>
-            <small class="text-success fw-semibold">Dokumen</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="fw-semibold d-block mb-1">Tesis</span>
-            </div>
-            
-            <h3 class="card-title mb-2">105</h3>
-            <small class="text-success fw-semibold">Dokumen</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="card-title d-flex align-items-start justify-content-between">
-                <span class="fw-semibold d-block mb-1">Disertasi</span>
-            </div>
-            
-            <h3 class="card-title mb-2">42</h3>
-            <small class="text-success fw-semibold">Dokumen</small>
-          </div>
-        </div>
-      </div>
-      <!-- </div>
-    <div class="row"> -->
-      <div class="col-12 mb-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
-              <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
-                <div class="card-title">
-                  <h5 class="text-nowrap mb-2">Profile Report</h5>
-                  <span class="badge bg-label-warning rounded-pill">Year 2021</span>
-                </div>
-                <div class="mt-sm-auto">
-                  <small class="text-success text-nowrap fw-semibold"><i class='bx bx-chevron-up'></i> 68.2%</small>
-                  <h3 class="mb-0">$84,686k</h3>
-                </div>
+            {{-- <div class="dropdown">
+              <button class="btn p-0" type="button" id="orederStatistics" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="bx bx-dots-vertical-rounded"></i>
+              </button>
+              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="orederStatistics">
+                <a class="dropdown-item" href="javascript:void(0);">Select All</a>
+                <a class="dropdown-item" href="javascript:void(0);">Refresh</a>
+                <a class="dropdown-item" href="javascript:void(0);">Share</a>
               </div>
-              <div id="profileReportChart"></div>
+            </div> --}}
+          </div>
+          <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <div class="d-flex flex-column align-items-center gap-1">
+                <h2 class="mb-2">258</h2>
+                <span>Total Dokumen</span>
+              </div>
+              <div id="orderStatisticsChart"></div>
             </div>
+            <ul class="p-0 m-0">
+              <li class="d-flex mb-4 pb-1">
+                <div class="avatar flex-shrink-0 me-3">
+                  <span class="avatar-initial rounded bg-label-primary">S</span>
+                </div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                  <div class="me-2">
+                    <h6 class="mb-0">Skripsi</h6>
+                    {{-- <small class="text-muted">Mobile, Earbuds, TV</small> --}}
+                  </div>
+                  <div class="user-progress">
+                    <small class="fw-semibold">282</small>
+                  </div>
+                </div>
+              </li>
+              <li class="d-flex mb-4 pb-1">
+                <div class="avatar flex-shrink-0 me-3">
+                  <span class="avatar-initial rounded bg-label-success">T</span>
+                </div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                  <div class="me-2">
+                    <h6 class="mb-0">Tesis</h6>
+                    {{-- <small class="text-muted">T-shirt, Jeans, Shoes</small> --}}
+                  </div>
+                  <div class="user-progress">
+                    <small class="fw-semibold">57</small>
+                  </div>
+                </div>
+              </li>
+              <li class="d-flex mb-4 pb-1">
+                <div class="avatar flex-shrink-0 me-3">
+                  <span class="avatar-initial rounded bg-label-info">D</span>
+                </div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                  <div class="me-2">
+                    <h6 class="mb-0">Disertasi</h6>
+                    {{-- <small class="text-muted">Fine Art, Dining</small> --}}
+                  </div>
+                  <div class="user-progress">
+                    <small class="fw-semibold">84</small>
+                  </div>
+                </div>
+              </li>
+              <li class="d-flex mb-4 pb-1">
+                <div class="avatar flex-shrink-0 me-3">
+                  <span class="avatar-initial rounded bg-label-secondary">TA</span>
+                </div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                  <div class="me-2">
+                    <h6 class="mb-0">Tugas Akhir</h6>
+                    {{-- <small class="text-muted">Football, Cricket Kit</small> --}}
+                  </div>
+                  <div class="user-progress">
+                    <small class="fw-semibold">99</small>
+                  </div>
+                </div>
+              </li>
+              <li class="d-flex mb-4 pb-1">
+                <div class="avatar flex-shrink-0 me-3">
+                  <span class="avatar-initial rounded bg-label-warning">A</span>
+                </div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                  <div class="me-2">
+                    <h6 class="mb-0">Artikel</h6>
+                    {{-- <small class="text-muted">Football, Cricket Kit</small> --}}
+                  </div>
+                  <div class="user-progress">
+                    <small class="fw-semibold">35</small>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
+    </div>
+    <div class="row">
+      
     </div>
   </div>
 </div>
 <div class="row">
   <!-- Order Statistics -->
-  <div class="col-md-6 col-lg-4 col-xl-4 order-0 mb-4">
-    <div class="card h-100">
-      <div class="card-header d-flex align-items-center justify-content-between pb-0">
-        <div class="card-title mb-0">
-          <h5 class="m-0 me-2">Order Statistics</h5>
-          <small class="text-muted">42.82k Total Sales</small>
-        </div>
-        <div class="dropdown">
-          <button class="btn p-0" type="button" id="orederStatistics" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="bx bx-dots-vertical-rounded"></i>
-          </button>
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="orederStatistics">
-            <a class="dropdown-item" href="javascript:void(0);">Select All</a>
-            <a class="dropdown-item" href="javascript:void(0);">Refresh</a>
-            <a class="dropdown-item" href="javascript:void(0);">Share</a>
-          </div>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="d-flex justify-content-between align-items-center mb-3">
-          <div class="d-flex flex-column align-items-center gap-1">
-            <h2 class="mb-2">8,258</h2>
-            <span>Total Orders</span>
-          </div>
-          <div id="orderStatisticsChart"></div>
-        </div>
-        <ul class="p-0 m-0">
-          <li class="d-flex mb-4 pb-1">
-            <div class="avatar flex-shrink-0 me-3">
-              <span class="avatar-initial rounded bg-label-primary"><i class='bx bx-mobile-alt'></i></span>
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Electronic</h6>
-                <small class="text-muted">Mobile, Earbuds, TV</small>
-              </div>
-              <div class="user-progress">
-                <small class="fw-semibold">82.5k</small>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex mb-4 pb-1">
-            <div class="avatar flex-shrink-0 me-3">
-              <span class="avatar-initial rounded bg-label-success"><i class='bx bx-closet'></i></span>
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Fashion</h6>
-                <small class="text-muted">T-shirt, Jeans, Shoes</small>
-              </div>
-              <div class="user-progress">
-                <small class="fw-semibold">23.8k</small>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex mb-4 pb-1">
-            <div class="avatar flex-shrink-0 me-3">
-              <span class="avatar-initial rounded bg-label-info"><i class='bx bx-home-alt'></i></span>
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Decor</h6>
-                <small class="text-muted">Fine Art, Dining</small>
-              </div>
-              <div class="user-progress">
-                <small class="fw-semibold">849k</small>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex">
-            <div class="avatar flex-shrink-0 me-3">
-              <span class="avatar-initial rounded bg-label-secondary"><i class='bx bx-football'></i></span>
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Sports</h6>
-                <small class="text-muted">Football, Cricket Kit</small>
-              </div>
-              <div class="user-progress">
-                <small class="fw-semibold">99</small>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  
   <!--/ Order Statistics -->
 
   <!-- Expense Overview -->
-  <div class="col-md-6 col-lg-4 order-1 mb-4">
+  {{-- <div class="col-md-6 col-lg-4 order-1 mb-4">
     <div class="card h-100">
       <div class="card-header">
         <ul class="nav nav-pills" role="tablist">
@@ -351,11 +272,11 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
   <!--/ Expense Overview -->
 
   <!-- Transactions -->
-  <div class="col-md-6 col-lg-4 order-2 mb-4">
+  {{-- <div class="col-md-6 col-lg-4 order-2 mb-4">
     <div class="card h-100">
       <div class="card-header d-flex align-items-center justify-content-between">
         <h5 class="card-title m-0 me-2">Transactions</h5>
@@ -459,7 +380,7 @@
         </ul>
       </div>
     </div>
-  </div>
+  </div> --}}
   <!--/ Transactions -->
 </div>
 @endsection
